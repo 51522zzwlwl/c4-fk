936a93fcccc99a9d1a1021a76e4e3a9037bf39da
51522zzwlwl <514051673@qq.com>
2021-01-23 14:18:49 +0800

    第一题

--- week1/src/work1.java
+++ week1/src/work1.java
@@ -13,20 +13,21 @@ public class work1 {
               {
                   System.out.println("过轻");
               }
-            if(result>18.5&&result<25)
+            else if(result>18.5&&result<25)
             {
                   System.out.println("正常");
             }
-            if(result>25&&result<28)
+            else if (result>25&&result<28)
             {
                   System.out.println("过重");
             }
-            if(result>28&&result<32)
+            else if (result>28&&result<32)
             {
                   System.out.println("肥胖");
             }
-            else
-                  System.out.println("非常肥胖");
+            else {
+                System.out.println("非常肥胖");
+            }
         }
     }
 }
