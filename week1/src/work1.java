0921b0f483f7befe9b64676b74b415398bc020e2
51522zzwlwl <514051673@qq.com>
2021-01-23 14:11:55 +0800

    第一题

---
+++ week1/src/work1.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class work1 {
+    public static void main(String[] args) {
+        {
+            Scanner in = new Scanner(System.in);
+            System.out.println("输入体重（kg）：");
+            double w = in.nextDouble();
+            System.out.println("输入身高（m）：");
+            double h = in.nextDouble();
+            double result = w/(h*h);
+            if(result<18.5)
+              {
+                  System.out.println("过轻");
+              }
+            if(result>18.5&&result<25)
+            {
+                  System.out.println("正常");
+            }
+            if(result>25&&result<28)
+            {
+                  System.out.println("过重");
+            }
+            if(result>28&&result<32)
+            {
+                  System.out.println("肥胖");
+            }
+            else
+                  System.out.println("非常肥胖");
+        }
+    }
+}
